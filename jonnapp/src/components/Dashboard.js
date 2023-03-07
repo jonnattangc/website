a53eadafa34d5397da3ed1a6f1d8cc6ee0213dd7
Jonnattan Griffiths <jonnattan@gmail.com>
2023-03-07 20:31:35 +0000

    puebas de cors y csrf token

--- jonnapp/src/components/Dashboard.js
+++ jonnapp/src/components/Dashboard.js
@@ -1,15 +1,12 @@
 import React from 'react';
-import { Grid } from '@mui/material';
 import { EnrollChart } from './EnrollChart'
 
 class Dashboard extends React.Component {
     render() {
         return (
-            <Grid container spacing={2}>
-                <Grid item xs={12}>
-                    <EnrollChart />
-                </Grid>
-            </Grid>
+        <div className='App_Main'>
+            <EnrollChart />
+        </div>
         );
     }
 }
